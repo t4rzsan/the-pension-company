61d5cfbfee9b54330d7541507b78bc231d63930e
t4rzsan <jakob.christensen@gmail.com>
2021-02-23 23:54:57 +0100

    Add Kleisli composition for Disabled event

---
+++ kleisli.fsx
@@ -0,0 +1,20 @@
+module Kleisli =
+    type Optional =
+        static member Compose a b x =
+            match a x with
+            | Ok v -> b v
+            | Error v  -> Error v
+
+        static member Identity x =
+            Ok(x)
+
+    let tryCatch f x =
+        try
+            f x |> Ok
+        with
+        | ex -> Error ex.Message
+
+    let unit f x =
+        match f x with
+        | Ok _ -> Ok()
+        | Error s -> Error s
